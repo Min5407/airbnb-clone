356edf2217cc9dd6e3307c63de1552fa9437d42c
Min Young Go <gmy5407@gmail.com>
2023-09-03 14:49:31 +0900

    feat: add basic design system

---
+++ src/components/navigation/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export const Navbar = () => {
+  return (
+    <nav className="">
+      <h1>
+        <Link href="/" className="text-large">
+          dadddsdfw
+        </Link>
+      </h1>
+    </nav>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,25 +3,40 @@ import { createThemes } from 'tw-colors'
 
 const config: Config = {
   content: ['./src/components/**/*.{ts,tsx}', './src/app/**/*.{ts,tsx}'],
+  theme: {
+    fontSize: {
+      tiny: 'clamp(12px, 1.5vw, 14px)',
+      small: 'clamp(14px, 2vw, 16px)',
+      base: 'clamp(16px, 2.5vw, 20px)',
+      large: 'clamp(20px, 3vw, 24px)',
+      xlarge: 'clamp(24px, 3.5vw, 28px)',
+      xxlarge: 'clamp(28px, 4vw, 32px)',
+      xxxlarge: 'clamp(32px, 4.5vw, 36px)',
+    },
+
+    extend: {
+      spacing: {
+        tiny: '0.25rem',
+        small: '0.5rem',
+        base: '0.75rem',
+        large: '1rem',
+        xLarge: '2rem',
+      },
+    },
+  },
 
   plugins: [
     createThemes({
       default: {
-        default: {
-          brandColors: {
-            primary: '#7766c6',
-            darkPrimary: '#46467A',
-            lightPrimary: '#E0DFFD',
-            secondary: '#FFC212',
-            tertiary: '#F9B0C3',
-          },
-          bgColors: {
-            dark: '#333',
-            light: '#f9f9f9',
-          },
-          white: '#fff',
-          dark: '#000',
+        brandColors: {
+          red: '#FF5A5F',
+          green: '#00A699',
+          orange: '#FC642D',
+          dark: '#484848',
+          gray: '#767676',
         },
+        white: '#fff',
+        dark: '#000',
       },
     }),
   ],
